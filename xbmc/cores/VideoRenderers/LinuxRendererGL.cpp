f315816b0e5a683fd723ecd0de56ec93fc77088e
Gregor Fuis <gregor.fuis@gmail.com>
2012-08-29 22:10:23 +0200

    902.12-linuxrenderer_delete_texture_targets_on_reconfigure.patch

--- xbmc/cores/VideoRenderers/LinuxRendererGL.cpp
+++ xbmc/cores/VideoRenderers/LinuxRendererGL.cpp
@@ -252,6 +252,12 @@ bool CLinuxRendererGL::ValidateRenderTarget()
     for (int i = 0 ; i < m_NumYV12Buffers ; i++)
       (this->*m_textureDelete)(i);
 
+    // function pointer for texture might change in
+    // call to LoadShaders
+    glFinish();
+    for (int i = 0 ; i < m_NumYV12Buffers ; i++)
+      (this->*m_textureDelete)(i);
+
      // create the yuv textures
     LoadShaders();
 
